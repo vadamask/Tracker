bc797c65fcfc730d7011a52a083762eb0f6ee9c8
Vadim Shishkov <vadamask@gmail.com>
2023-09-01 20:32:49 +0300

    Create empty statistics screen

--- Tracker/Statistics/StatisticsViewController.swift
+++ Tracker/Statistics/StatisticsViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 final class StatisticsViewController: UIViewController {
     
+    private let placeholder: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "empty statistics"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Анализировать пока нечего"
+        label.textColor = .blackYP
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -17,10 +33,20 @@ final class StatisticsViewController: UIViewController {
     
     private func setupViews() {
         view.backgroundColor = .whiteYP
+        navigationItem.title = "Статистика"
         
     }
     
     private func setupConstraints() {
+        view.addSubview(placeholder)
+        view.addSubview(label)
         
+        NSLayoutConstraint.activate([
+            placeholder.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeholder.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.topAnchor.constraint(equalTo: placeholder.bottomAnchor, constant: 8),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
